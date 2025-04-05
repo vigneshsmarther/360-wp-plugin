5f00543449d3a7d56a0ceebf583bcab7b0938351
newsintv <newsintv.com@gmail.com>
2025-04-05 08:15:21 +0530

    Add files via upload

--- templates/preview-template.php
+++ templates/preview-template.php
@@ -1,1 +1,43 @@
+<?php
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+?>
+<section class="truck-360-preview">
+    <div class="preview-container">
+        <div class="preview-row">
+            <div class="preview-info-column">
+                <h3 class="preview-title"><?php echo esc_html($atts['title']); ?><span class="red-dot"></span></h3>
+                <p class="preview-description"><?php echo esc_html($atts['description']); ?></p>
+                <div class="preview-alert">
+                    <span>Maximum upload size 5MB</span>
+                </div>
+                <div class="preview-buttons">
+                    <a id="btn-preview-sides" class="preview-button" href="#">UPLOAD SIDES</a>
+                    <input type="file" id="preview_file_sides" class="hidden-file-input">
 
+                    <a id="btn-preview-back" class="preview-button" href="#">UPLOAD BACK</a>
+                    <input type="file" id="preview_file_back" class="hidden-file-input">
+                </div>
+            </div>
+            <div class="preview-viewer-column">
+                <div class="preview-360-holder">
+                    <div id="preview-360" class="three-sixty-image-holder"
+                         data-url="<?php echo plugin_dir_url(dirname(__FILE__)) . 'assets/images/360/'; ?>"
+                         data-imgformat="jpg"
+                         data-imgcount="18">
+                        <?php for ($i = 1; $i <= 18; $i++) : ?>
+                            <img draggable="false" 
+                                 src="<?php echo plugin_dir_url(dirname(__FILE__)) . 'assets/images/360/' . $i . '.jpg'; ?>" 
+                                 style="width: 100%; height: 100%; object-fit: contain; <?php echo $i > 1 ? 'display: none;' : 'display: block;'; ?>">
+                        <?php endfor; ?>
+                    </div>
+                    <div class="loader js-360-loader">
+                        <img src="<?php echo plugin_dir_url(dirname(__FILE__)) . 'assets/images/loader.gif'; ?>" alt="loader">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
